a4609a62c59050060268b0a7f349413121fd6ae3
Dexxcuyy <deodeki40@gmail.com>
2026-06-29 02:59:16 +0700

    Refactor run function in Deploy contract
    Changed the visibility of the run function to public and modified its return type to DexxLauncher.

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,19 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import "forge-std/Script.sol";
 import "../src/DexxLauncher.sol";
 
-contract Deploy is Script {
+contract Deploy {
     address constant FACTORY =
         0xE85A59c628F7d27878ACeB4bf3b35733630083a9;
 
-    function run() external {
-        vm.startBroadcast();
-
-        new DexxLauncher(FACTORY);
-
-        vm.stopBroadcast();
+    function run() public returns (DexxLauncher launcher) {
+        launcher = new DexxLauncher(FACTORY);
     }
 }
-
